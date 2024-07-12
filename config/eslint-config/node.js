c55b4cf417524d78e0bdd28414166d04ac8a6d79
Alessandro0003 <alessandroquinta3@gmail.com>
2024-07-11 23:18:48 -0300

    🔥 feat: configuration eslint to project

--- config/eslint-config/node.js
+++ config/eslint-config/node.js
@@ -1,7 +1,7 @@
 /**  @type {import('eslint').Linter.Config} */
 
 module.exports = {
-  extends: ['@rocketseat/eslint-config/next'],
+  extends: ['@rocketseat/eslint-config/node'],
   plugins: ['simple-import-sort'],
   rules: {
     'simple-import-sort/imports': 'error',
