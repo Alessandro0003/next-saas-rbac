766e4d05e8d85c5df274103841aff2d992faf1d1
Alessandro0003 <alessandroquinta3@gmail.com>
2024-08-01 16:54:43 -0300

    🔥feat: create new route requestPasswordRecover

---
+++ apps/api/src/http/routes/auth/request-password-recover.ts
@@ -0,0 +1,50 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+export async function requestPasswordRecover(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/password/recover',
+    {
+      schema: {
+        tags: ['password-recover'],
+        summary: 'Password recover',
+        body: z.object({
+          email: z.string().email(),
+        }),
+        response: {
+          201: z.null(),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { email } = request.body
+
+      const userFromEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (!userFromEmail) {
+        // will be returned this way because, we don't want people to know if the user actually exists
+        return reply.status(201).send()
+      }
+
+      const { id: code } = await prisma.token.create({
+        data: {
+          type: 'PASSWORD_RECOVER',
+          userId: userFromEmail.id,
+        },
+      })
+
+      // Send e-mail with password recover link
+
+      console.log('Recover password token: ', code)
+
+      return reply.status(201).send()
+    },
+  )
+}
