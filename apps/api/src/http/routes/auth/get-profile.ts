acf44e514340ef188ca8bd4674e40612d9df63dd
Alessandro0003 <alessandroquinta3@gmail.com>
2024-08-01 16:21:56 -0300

    :fireworks: feat: passing middleware auth

--- apps/api/src/http/routes/auth/get-profile.ts
+++ apps/api/src/http/routes/auth/get-profile.ts
@@ -2,49 +2,53 @@ import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 
+import { auth } from '@/http/middlewares/auth'
 import { prisma } from '@/lib/prisma'
 
 import { BadRequestError } from '../_errors/bad-request-error'
 
 export async function getProfile(app: FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>().get(
-    '/profile',
-    {
-      schema: {
-        tags: ['profile'],
-        summary: 'Get authenticated user profile',
-        response: {
-          200: z.object({
-            user: z.object({
-              id: z.string().uuid(),
-              name: z.string().nullable(),
-              email: z.string().email(),
-              avatarUtl: z.string().url().nullable(),
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/profile',
+      {
+        schema: {
+          tags: ['profile'],
+          summary: 'Get authenticated user profile',
+          response: {
+            200: z.object({
+              user: z.object({
+                id: z.string().uuid(),
+                name: z.string().nullable(),
+                email: z.string().email(),
+                avatarUtl: z.string().url().nullable(),
+              }),
             }),
-          }),
+          },
         },
       },
-    },
-    async (request, reply) => {
-      const { sub } = await request.jwtVerify<{ sub: string }>()
+      async (request, reply) => {
+        const userId = await request.getCurrentUserId()
 
-      const user = await prisma.user.findFirst({
-        select: {
-          id: true,
-          name: true,
-          email: true,
-          avatarUtl: true,
-        },
-        where: {
-          id: sub,
-        },
-      })
+        const user = await prisma.user.findFirst({
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            avatarUtl: true,
+          },
+          where: {
+            id: userId,
+          },
+        })
 
-      if (!user) {
-        throw new BadRequestError('User not found!')
-      }
+        if (!user) {
+          throw new BadRequestError('User not found!')
+        }
 
-      return reply.status(200).send({ user })
-    },
-  )
+        return reply.status(200).send({ user })
+      },
+    )
 }
