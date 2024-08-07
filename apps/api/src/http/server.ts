e5d1bdd8b6d6e9a6cc94c411743f1822ec6ee76b
Alessandro0003 <alessandroquinta3@gmail.com>
2024-08-06 21:45:08 -0300

    :fireworks: feat: creation new routes for organization as getOrganizationDetails and getOrganizations

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -20,6 +20,8 @@ import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
 import { createOrganization } from './routes/orgs/create-organization'
 import { getMembership } from './routes/orgs/get-membership'
+import { getOrganizationDetails } from './routes/orgs/get-organization-details'
+import { getOrganizations } from './routes/orgs/get-organizations'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -66,6 +68,8 @@ app.register(authenticateWithGithub)
 
 app.register(createOrganization)
 app.register(getMembership)
+app.register(getOrganizationDetails)
+app.register(getOrganizations)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running🔥🚀')
