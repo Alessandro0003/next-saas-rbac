1b8c79c1439282000e81b47e7c887fba3e33272f
Alessandro0003 <alessandroquinta3@gmail.com>
2024-08-06 21:09:24 -0300

    :fireworks: feat: export method role

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -17,6 +17,7 @@ import { userSubject } from './subjects/user'
 export * from './models/organization'
 export * from './models/project'
 export * from './models/user'
+export * from './role'
 
 // type AppAbilities = UserSubject | ProjectSubject | ['manage', 'all']
 
