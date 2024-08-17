32a5c6ed6893a1783fde44efaa11d74a6dec8349
Alessandro0003 <alessandroquinta3@gmail.com>
2024-08-16 22:02:38 -0300

    :sparkles: feat: add new route

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -22,6 +22,7 @@ import { createOrganization } from './routes/orgs/create-organization'
 import { getMembership } from './routes/orgs/get-membership'
 import { getOrganizationDetails } from './routes/orgs/get-organization-details'
 import { getOrganizations } from './routes/orgs/get-organizations'
+import { shutdownOrganization } from './routes/orgs/shutdown-organization'
 import { updateOrganization } from './routes/orgs/update-organization'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -72,6 +73,7 @@ app.register(getMembership)
 app.register(getOrganizationDetails)
 app.register(getOrganizations)
 app.register(updateOrganization)
+app.register(shutdownOrganization)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running🔥🚀')
