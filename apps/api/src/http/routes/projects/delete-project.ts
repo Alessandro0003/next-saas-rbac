ead1a545a6f651ed505c4e7dccecddedfc29ea91
Alessandro0003 <alessandroquinta3@gmail.com>
2024-08-18 20:45:18 -0300

    :fire: feat: creating new route to Delete Project

---
+++ apps/api/src/http/routes/projects/delete-project.ts
@@ -0,0 +1,67 @@
+import { projectSchema } from '@saas/auth'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+
+import { UnauthorizedError } from '../_errors/unauthorized-error'
+
+export async function deleteProject(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .delete(
+      '/organizations/:slug/projects/:projectId',
+      {
+        schema: {
+          tags: ['Projects'],
+          summary: 'Delete a project',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+            projectId: z.string(),
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug, projectId } = request.params
+        const userId = await request.getCurrentUserId()
+        const { organization, membership } =
+          await request.getUserMembership(slug)
+
+        const project = await prisma.project.findUnique({
+          where: {
+            id: projectId,
+            organizationId: organization.id,
+          },
+        })
+
+        if (!project) {
+          throw new UnauthorizedError('project not found')
+        }
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+        const authProject = projectSchema.parse(project)
+
+        if (cannot('delete', authProject)) {
+          throw new UnauthorizedError(
+            `You're not allowed to delete this project.`,
+          )
+        }
+
+        await prisma.project.delete({
+          where: {
+            id: projectId,
+          },
+        })
+
+        return reply.status(204).send()
+      },
+    )
+}
