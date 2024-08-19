6a7ae40742531a05a2cef5f0c0c733ed5b7189dc
Alessandro0003 <alessandroquinta3@gmail.com>
2024-08-19 00:30:46 -0300

    :fire: feat: creation new route to updateMember

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -19,6 +19,8 @@ import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
 import { getMembers } from './routes/members/get-members'
+import { removeMember } from './routes/members/remove-member'
+import { updateMember } from './routes/members/update-members'
 import { createOrganization } from './routes/orgs/create-organization'
 import { getMembership } from './routes/orgs/get-membership'
 import { getOrganizationDetails } from './routes/orgs/get-organization-details'
@@ -90,6 +92,8 @@ app.register(getProjects)
 app.register(updateProject)
 
 app.register(getMembers)
+app.register(updateMember)
+app.register(removeMember)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running🔥🚀')
