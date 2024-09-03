7d17975c1379856b8c8da33e2ee99e80d3ab928b
Alessandro0003 <alessandroquinta3@gmail.com>
2024-09-02 23:21:13 -0300

    :sparkles: feat: creation screen to signIn

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <div>
+      <Button>SignIn</Button>
+    </div>
+  )
 }
