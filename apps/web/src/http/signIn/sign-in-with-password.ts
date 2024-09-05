400c44fea75882349a46212d2e22d2e8dee9b84e
Alessandro0003 <alessandroquinta3@gmail.com>
2024-09-04 23:06:30 -0300

    :sparkles: feat: creation function method http to signInWithPassword

---
+++ apps/web/src/http/signIn/sign-in-with-password.ts
@@ -0,0 +1,25 @@
+import { api } from '@/http/api-client'
+
+interface SignInWithPasswordRequest {
+  email: string
+  password: string
+}
+interface SignInWithPasswordResponse {
+  token: string
+}
+
+export async function signInWithPassword({
+  email,
+  password,
+}: SignInWithPasswordRequest) {
+  const result = await api
+    .post('sessions/password', {
+      json: {
+        email,
+        password,
+      },
+    })
+    .json<SignInWithPasswordResponse>()
+
+  return result
+}
