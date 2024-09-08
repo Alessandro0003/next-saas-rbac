ce42b45d60905eff83639f5215d33672973c7b82
Alessandro0003 <alessandroquinta3@gmail.com>
2024-09-08 00:07:37 -0300

    :fire: feat: creation signIn with github

---
+++ apps/web/src/app/auth/action.ts
@@ -0,0 +1,17 @@
+'use server'
+
+import { redirect } from 'next/navigation'
+
+export async function signInWithGithub() {
+  const githubSignInURL = new URL('login/oauth/authorize', 'https://github.com')
+  // const githubSignInURL = new URL('login/oauth/authorize', 'https://github.com')
+
+  githubSignInURL.searchParams.set('client_id', 'Ov23ct08oQtA6ZEqjz1Q')
+  githubSignInURL.searchParams.set(
+    'redirect_uri',
+    'http://localhost:3000/api/auth/callback',
+  )
+  githubSignInURL.searchParams.set('scope', 'user')
+
+  redirect(githubSignInURL.toString())
+}
---
+++ apps/web/src/http/signIn/sign-in-with-github.ts
@@ -0,0 +1,20 @@
+import { api } from '@/http/api-client'
+
+interface SignInWithGithubRequest {
+  code: string
+}
+interface SignInWithGithubResponse {
+  token: string
+}
+
+export async function signInWithGithub({ code }: SignInWithGithubRequest) {
+  const result = await api
+    .post('sessions/github', {
+      json: {
+        code,
+      },
+    })
+    .json<SignInWithGithubResponse>()
+
+  return result
+}
