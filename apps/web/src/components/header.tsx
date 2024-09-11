14e74d0033601d3d9cb2457e1a96c3881c8f46a7
Alessandro0003 <alessandroquinta3@gmail.com>
2024-09-10 23:39:31 -0300

    :sparkles: feat: add and styles components to header

---
+++ apps/web/src/components/header.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+
+import logoHeader from '@/assets/logo-header.svg'
+
+import { ProfileButton } from './profile-button'
+export function Header() {
+  return (
+    <div className="mx-auto flex max-w-[1200px] items-center justify-between">
+      <div className="flex items-center gap-3">
+        <Image src={logoHeader} className="size-6 dark:invert" alt="home" />
+      </div>
+      <div className="flex items-center gap-4">
+        <ProfileButton />
+      </div>
+    </div>
+  )
+}
