c15435d0020d138cfc24a7588e1284c114a4c9d3
Alessandro0003 <alessandroquinta3@gmail.com>
2024-09-16 20:33:05 -0300

    :sparkles: feat: creation method to take initials user name

---
+++ apps/web/src/lib/get-initials-name.ts
@@ -0,0 +1,8 @@
+export function getInitialsName(name: string): string {
+  const initials = name
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase())
+    .slice(0, 2)
+    .join('')
+  return initials
+}
