b4f1056e318b2ec9b33528134afa7a8de55a909b
Alessandro0003 <alessandroquinta3@gmail.com>
2024-09-19 21:49:46 -0300

    :fireworks: feat: passing new component to header

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -6,6 +6,8 @@ import { ability } from '@/auth/auth'
 
 import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
+import { ThemeSwitcher } from './theme/theme-switcher'
+import { Separator } from './ui/separator'
 
 export async function Header() {
   const permissions = await ability()
@@ -20,6 +22,8 @@ export async function Header() {
         {permissions?.can('get', 'Project') && <p>Projetos</p>}
       </div>
       <div className="flex items-center gap-4">
+        <ThemeSwitcher />
+        <Separator orientation="vertical" className="h-5" />
         <ProfileButton />
       </div>
     </div>
