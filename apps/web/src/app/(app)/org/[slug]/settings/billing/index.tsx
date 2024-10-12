bd9fe49b9c0004e10f12bb893377ab39269c061b
Alessandro0003 <alessandroquinta3@gmail.com>
2024-10-11 22:15:37 -0300

    :sparkles: feat: finalizing billing table

--- apps/web/src/app/(app)/org/[slug]/settings/billing/index.tsx
+++ apps/web/src/app/(app)/org/[slug]/settings/billing/index.tsx
@@ -11,6 +11,7 @@ import {
   Table,
   TableBody,
   TableCell,
+  TableFooter,
   TableHead,
   TableHeader,
   TableRow,
@@ -36,13 +37,15 @@ export async function Billing() {
         <CardContent>
           <Table>
             <TableHeader>
-              <TableHead>Cost type</TableHead>
-              <TableHead className="text-right" style={{ width: 120 }}>
-                Quantity
-              </TableHead>
-              <TableHead className="text-right" style={{ width: 120 }}>
-                Subtotal
-              </TableHead>
+              <TableRow>
+                <TableHead>Cost type</TableHead>
+                <TableHead className="text-right" style={{ width: 120 }}>
+                  Quantity
+                </TableHead>
+                <TableHead className="text-right" style={{ width: 160 }}>
+                  Subtotal
+                </TableHead>
+              </TableRow>
             </TableHeader>
 
             <TableBody>
@@ -52,10 +55,50 @@ export async function Billing() {
                   {billing.projects.amount}
                 </TableCell>
                 <TableCell className="text-right">
-                  {billing.projects.price} ({billing.projects.unit} each)
+                  {billing.projects.price.toLocaleString('en-US', {
+                    style: 'currency',
+                    currency: 'USD',
+                  })}{' '}
+                  (
+                  {billing.projects.unit.toLocaleString('en-US', {
+                    style: 'currency',
+                    currency: 'USD',
+                  })}{' '}
+                  each)
+                </TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell>Amount of seats</TableCell>
+                <TableCell className="text-right">
+                  {billing.seats.amount}
+                </TableCell>
+                <TableCell className="text-right">
+                  {billing.seats.price.toLocaleString('en-US', {
+                    style: 'currency',
+                    currency: 'USD',
+                  })}{' '}
+                  (
+                  {billing.seats.unit.toLocaleString('en-US', {
+                    style: 'currency',
+                    currency: 'USD',
+                  })}{' '}
+                  each)
                 </TableCell>
               </TableRow>
             </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TableCell />
+                <TableCell className="text-right">Total</TableCell>
+                <TableCell className="text-right">
+                  {billing.total.toLocaleString('en-US', {
+                    style: 'currency',
+                    currency: 'USD',
+                  })}{' '}
+                </TableCell>
+              </TableRow>
+            </TableFooter>
           </Table>
         </CardContent>
       </Card>
