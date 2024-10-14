f940e8e849731bd0b71664a7333f795c9e917b8a
Alessandro0003 <alessandroquinta3@gmail.com>
2024-10-13 21:05:16 -0300

    :fire: feat: create nem action to updateMember

--- apps/web/src/app/(app)/org/[slug]/members/actions/index.ts
+++ apps/web/src/app/(app)/org/[slug]/members/actions/index.ts
@@ -1,15 +1,28 @@
 'use server'
 
+import { Role } from '@saas/auth'
 import { revalidateTag } from 'next/cache'
 
 import { getCurrentOrg } from '@/auth/auth'
 import { removeMembers } from '@/http/members/remove-members'
+import { updateMembers } from '@/http/members/update-member'
 
 export async function removeMemberAction(memberId: string) {
   const currentOrg = getCurrentOrg()
   await removeMembers({
     org: currentOrg!,
     memberId,
+  })
+
+  revalidateTag(`${currentOrg}/members`)
+}
+
+export async function updateMemberAction(memberId: string, role: Role) {
+  const currentOrg = getCurrentOrg()
+  await updateMembers({
+    org: currentOrg!,
+    memberId,
+    role,
   })
 
   revalidateTag(`${currentOrg}/members`)
