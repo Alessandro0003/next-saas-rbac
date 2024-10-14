c78623a7eb7bbc58f7b423ab4fedc31abb9c84ca
Alessandro0003 <alessandroquinta3@gmail.com>
2024-10-13 23:41:16 -0300

    :sparkles: feat: create new component RevokeInviteButton

---
+++ apps/web/src/app/(app)/org/[slug]/members/components/revoke-invite-button.tsx
@@ -0,0 +1,21 @@
+import { XOctagon } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+
+import { revokeInviteAction } from '../actions'
+
+interface RevokeInviteButtonProps {
+  inviteId: string
+}
+
+export async function RevokeInviteButton(props: RevokeInviteButtonProps) {
+  const { inviteId } = props
+  return (
+    <form action={revokeInviteAction.bind(null, inviteId)}>
+      <Button size="sm" variant="destructive">
+        <XOctagon className="mr-2 size-4" />
+        Revoke invite
+      </Button>
+    </form>
+  )
+}
