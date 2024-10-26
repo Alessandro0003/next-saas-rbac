2ba3377f0f7ebbaeb75a99eed7083b3dc7c4e200
Alessandro0003 <alessandroquinta3@gmail.com>
2024-10-26 18:40:42 -0300

    :rocket: feat: create new method to getInvite

---
+++ apps/web/src/http/invites/get-invite.ts
@@ -0,0 +1,28 @@
+import { Role } from '@saas/auth'
+
+import { api } from '../api-client'
+
+interface GetInviteResponse {
+  invite: {
+    id: string
+    role: Role
+    email: string
+    createdAt: string
+    organization: {
+      name: string
+    }
+    author: {
+      id: string
+      name: string | null
+      avatarUrl: string | null
+    } | null
+  }
+}
+
+export async function getInvite(inviteId: string) {
+  const result = await api
+    .get(`invites/${inviteId}/details`)
+    .json<GetInviteResponse>()
+
+  return result
+}
