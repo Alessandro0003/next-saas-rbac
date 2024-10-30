48ef95d84b61906e788d5e47c8c3cf9a0acc1b99
Alessandro0003 <alessandroquinta3@gmail.com>
2024-10-29 23:56:27 -0300

    :fire: feat(InvtesActions): create actions to invites Accept and Reject

---
+++ apps/web/src/components/pending-invites/action.ts
@@ -0,0 +1,14 @@
+import { revalidateTag } from 'next/cache'
+
+import { acceptInvite } from '@/http/invites/accept-invite'
+import { rejectInvite } from '@/http/invites/reject-invite'
+
+export async function acceptInviteAction(inviteId: string) {
+  await acceptInvite(inviteId)
+
+  revalidateTag('organizations')
+}
+
+export async function rejectInviteAction(inviteId: string) {
+  await rejectInvite(inviteId)
+}
