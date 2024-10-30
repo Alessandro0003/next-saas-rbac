4b8de6439916d5c875ebe93d13624f2613a874ba
Alessandro0003 <alessandroquinta3@gmail.com>
2024-10-29 23:58:25 -0300

    :construction: feat(PopoverInvites): create setup to popover invites

--- apps/web/src/components/pending-invites/index.tsx
+++ apps/web/src/components/pending-invites/index.tsx
@@ -1,14 +1,43 @@
+'use client'
+
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import { Check, UserPlus2, X } from 'lucide-react'
+import { useState } from 'react'
+
+import { getPendingInvites } from '@/http/invites/get-pending-invites'
 
 import { Button } from '../ui/button'
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
+import { acceptInviteAction, rejectInviteAction } from './action'
 
 dayjs.extend(relativeTime)
+
 export function PendingInvites() {
+  const queryClient = useQueryClient()
+  const [isOpen, setIsOpen] = useState(false)
+
+  const { data, isLoading, refetch } = useQuery({
+    queryKey: ['pending-invites'],
+    queryFn: getPendingInvites,
+    enabled: isOpen,
+  })
+
+  async function handleAcceptInvite(inviteId: string) {
+    await acceptInviteAction(inviteId)
+
+    queryClient.invalidateQueries({ queryKey: ['pending-invites'] })
+  }
+
+  async function handleRejectInvite(inviteId: string) {
+    await rejectInviteAction(inviteId)
+
+    queryClient.invalidateQueries({ queryKey: ['pending-invites'] })
+  }
+
   return (
-    <Popover>
+    <Popover open={isOpen} onOpenChange={setIsOpen}>
       <PopoverTrigger>
         <Button size="icon" variant="ghost">
           <UserPlus2 className="size-4" />
@@ -17,28 +46,47 @@ export function PendingInvites() {
       </PopoverTrigger>
 
       <PopoverContent className="w-80 space-y-2">
-        <span className="block text-sm font-medium">Pending invites (2)</span>
+        <span className="block text-sm font-medium">
+          Pending invites ({data?.invites.length ?? 0})
+        </span>
 
-        <div className="space-y-2">
-          <p className="text-sm leading-relaxed text-muted-foreground">
-            <span className="font-medium text-foreground">John Doe</span>{' '}
-            invited you to join{' '}
-            <span className="font-medium text-foreground">Acme Inc</span>{' '}
-            <span>{dayjs(new Date()).fromNow()}</span>
-          </p>
+        {data?.invites.map((invite) => {
+          return (
+            <div className="space-y-2">
+              <p className="text-sm leading-relaxed text-muted-foreground">
+                <span className="font-medium text-foreground">
+                  {invite.author?.name ?? 'Someone'}
+                </span>{' '}
+                invited you to join{' '}
+                <span className="font-medium text-foreground">
+                  {invite.organization.name}
+                </span>{' '}
+                <span>{dayjs(invite.createdAt).fromNow()}</span>
+              </p>
 
-          <div className="flex gap-2">
-            <Button size="xs" variant="outline">
-              <Check className="mr-1.5 size-3" />
-              Accept
-            </Button>
+              <div className="flex gap-2">
+                <Button
+                  onClick={() => handleAcceptInvite(invite.id)}
+                  size="xs"
+                  variant="outline"
+                >
+                  <Check className="mr-1.5 size-3" />
+                  Accept
+                </Button>
 
-            <Button size="xs" variant="ghost" className="text-muted-foreground">
-              <X className="mr-1.5 size-3" />
-              Revoke
-            </Button>
-          </div>
-        </div>
+                <Button
+                  onClick={() => handleRejectInvite(invite.id)}
+                  size="xs"
+                  variant="ghost"
+                  className="text-muted-foreground"
+                >
+                  <X className="mr-1.5 size-3" />
+                  Reject
+                </Button>
+              </div>
+            </div>
+          )
+        })}
       </PopoverContent>
     </Popover>
   )
